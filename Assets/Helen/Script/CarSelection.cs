7bb0ef70148df744940abef9766688bdeb05aa77
Helen Yohanes <helen.yohanes@abbgymnasiet.se>
2022-12-14 13:31:20 +0100

    Bilarna väljs

--- Assets/Helen/Script/CarSelection.cs
+++ Assets/Helen/Script/CarSelection.cs
@@ -8,7 +8,7 @@ public class CarSelection : MonoBehaviour
     [SerializeField] private Button previousButton;
     [SerializeField] private Button nextButton;
    
-    private static int currentCar;
+    public static int currentCar=0;
 
     private void Awake()
     {
@@ -20,6 +20,7 @@ public class CarSelection : MonoBehaviour
         previousButton.interactable = (_index != 0);
         nextButton.interactable = (_index != transform.childCount-1);
         Debug.Log(currentCar);
+        
         for (int i = 0; i < transform.childCount; i++)
         {
             transform.GetChild(i).gameObject.SetActive(i == _index);
